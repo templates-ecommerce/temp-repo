a8e1f094bcc68edd48698defa0de9d4adee7642f
itcourses1701f <itcoursesbooks@gmail.com>
2022-01-07 20:49:09 +0500

    01-07-22

--- site/static/javascript/social_links.js
+++ site/static/javascript/social_links.js
@@ -1,11 +1,11 @@
-var fb = "https://fb.com";
-var tw = "https://twitter.com/login/";
-var yt = "https://youtube.com/";
-var insta = "https://instagram.com";
-var pt = "https://pinterest.com";
+var fb = "";
+var tw = "";
+var yt = "";
+var insta = "";
+var pt = "";
 
 
-var social_mobile = "923000000000";
+var social_mobile = "03210000000";
 var social_email = "example@gmail.com";
 var social_location = "address";
 var about_info = decodeURIComponent("At brand, we draw our inspirations from the diverse global arena and pack it under one roof, to share the fact that beauty is universal. We constantly strive to challenge the limitations of all the possibilities when it comes to weaving, printing, embroidery & embellishments. The cloth used is not mere fabric for us, we treat it like an artist treats his canvas; with the love, respect and dedication that it deserves. Here at Gul Ahmed, we understand that style and fashion are a vital part of your lives and so we strive to bring it to your doorstep in the easiest possible way. brand motto has always been to provide excellent quality and service.");
@@ -16,5 +16,5 @@ var company_name="Eralive";
 
 var tagmaneger = "GTM-MHJK6NC";
 
-var Receipt_Header = '%3bNTN+123456;ahmed170f@gmail.com;udam das tara 14 chand road karachi garden west'; 
+var Receipt_Header = ''; 
 var Receipt_Footer= ';No Refund;No Exchange';
